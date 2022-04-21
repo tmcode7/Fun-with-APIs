a25ed91e6fb4388ee5923912a49195652a4b7402
tmcode7 <mawuenatord7@gmail.com>
2022-04-21 01:43:36 +0000

    used an API to generate random animal names

--- Project 3/script.js
+++ Project 3/script.js
@@ -2,20 +2,28 @@ document.querySelector('button').addEventListener('click', getFetch)
 
 
 
-function getFetch(){
-    const satName = document.querySelector('input').value
-    const url = `http://tle.ivanstanojevic.me/api/tle?search=${satName}`
-   
+  function getFetch(){
+    const url = `https://zoo-animal-api.herokuapp.com/animals/rand`
   
     fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
-        console.log(data)
+        console.log(data.latin_name)
        
        
-        
+        document.querySelector('h1').innerText = data.name
+        document.querySelector('h3').innerText = data.latin_name
+        document.querySelector('section').innerText = data.image_link
+
+        const link = document.createElement('a')
+        //add text to the li
+        a.textContent = data.image_link
+        //append li to the ul
+        document.querySelector("a").appendChild(link)
+     
+      
       })
       .catch(err => {
           console.log(`error ${err}`)
       });
-  }
+  } 
